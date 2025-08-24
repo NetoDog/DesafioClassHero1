849abf20b3ef60437024ad4ded204923df54615a
NetoDog <hilariocancineto@gmail.com>
2025-08-24 18:17:32 -0300

    Desafio Class Hero
    Desafio Class hero

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string? nome;
+string? sobrenome;
+string? nomesobrenome;
+
+nome = Console.ReadLine();
+sobrenome = Console.ReadLine();
+
+Console.WriteLine(nome + " " +sobrenome);
+
+nomesobrenome = $"{nome} {sobrenome}";
+Console.WriteLine(nomesobrenome);
